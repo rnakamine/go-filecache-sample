af76475de95372aeb0968b47aad2f056b295f993
Ryo Nakamine <rnakamine8080@gmail.com>
2022-08-09 04:38:49 +0900

    Add cache

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/patrickmn/go-cache"
@@ -8,4 +9,15 @@ import (
 
 func main() {
 	c := cache.New(5*time.Minute, 10*time.Minute)
+
+	// Get cache
+	c.Set("foo", "bbbbbar", cache.NoExpiration)
+	c.SaveFile("cache")
+
+	// Set cache
+	c.LoadFile("cache")
+	raw, found := c.Get("foo")
+	if found {
+		fmt.Println(raw.(string))
+	}
 }
